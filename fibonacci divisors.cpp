6bff1f17e398efc56f217a27b5568162fe00193f
Ansarul14 <ansarul.islam117@gmail.com>
2017-07-18 02:31:19 +0600

    Add files via upload

---
+++ fibonacci divisors.cpp
@@ -0,0 +1,15 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll n,m,i,j,t;
+    cin>>t;
+    for(j=1;j<=t;j++){
+    cin>>n>>m;
+
+        i=(m/n);
+        cout<<i<<endl;
+    }
+}
